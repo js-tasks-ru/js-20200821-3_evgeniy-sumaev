0c4cc9bc7a4cfcf09fff8bebd9aa75b97b5cd845
Johny-S <9364016@gmail.com>
2020-08-30 00:19:25 +0300

    Решение задачи trimSymbols

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -4,6 +4,30 @@
  * @param {number} size - the allowed size of consecutive identical symbols
  * @returns {string} - the new string without extra symbols according passed size
  */
-export function trimSymbols(string, size) {
+export function trimSymbols(string, size = Infinity) {
+  const store = {
+    counter: 1,
+    prevLtr: '',
+    resultStr: '',
+    
+    ltrChecker(ltr) {
+      if (ltr === this.prevLtr) {
+        this.counter += 1;
+      } else {
+        this.counter = 1;
+      }
 
+      if (this.counter <= size) {
+        this.resultStr += ltr;
+      }
+      
+      this.prevLtr = ltr;
+    }
+  }
+
+  for (let i = 0; i < string.length; i += 1) {
+    store.ltrChecker(string[i]);
+  }
+
+  return store.resultStr;
 }
