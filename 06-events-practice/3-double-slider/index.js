d70089e753ae919e181dd07324383e7aa214266e
Johny-S <9364016@gmail.com>
2020-09-15 03:03:53 +0300

    Решение задачи 3-double-slider. V04

--- 06-events-practice/3-double-slider/index.js
+++ 06-events-practice/3-double-slider/index.js
@@ -1,212 +1,185 @@
-class Worker {
+class Handler {
 
   constructor(slider) {
-      // if (Worker.exist) {
-      //   Worker.exist.initValues(slider);  
-      //   return Worker.exist;
-      // }
-      // Worker.exist = this;
-      this.initValues(slider);
-  }
-
-  initValues(slider) {
     this.slider = slider;
   }
 
   handleEvent(event) {
-      this[event.type](event);
+    const handler = 'on' + event.type[0].toUpperCase() + event.type.slice(1);
+    this[handler](event);
   }
 
-  pointerdown(event) {
-    console.log(event);
-      const thumbElem = event.target.closest('[class^="range-slider__thumb"]');
-      
-      if(thumbElem) {
-        this.startPosition = event.clientX;
-        this.startOffsetX = event.offsetX;
-        this.currentThumb = thumbElem;
-        this.minPercent = this.slider.minPercent;
-        this.maxPercent = this.slider.maxPercent;
-      }
+  onPointerdown(event) {
+    event.preventDefault();
+    const thumbElem = event.target;
+    const { left, right } = thumbElem.getBoundingClientRect();
+    this.thumbSide = thumbElem.dataset.element.replace('Thumb', '');
+    const shift = {
+      left: right - event.clientX,
+      right: left - event.clientX,
+    }
+    this.shiftX = shift[this.thumbSide];
+
+    document.addEventListener('pointermove', this);
+    document.addEventListener('pointerup', this);
+    this.currentThumb = thumbElem;
   }
 
-  pointermove(event) {
-    const { minValue: minValueElem, maxValue: maxValueElem, progress: progressElem } = this.slider.subElements;
-    const { min, max, minPercent, maxPercent, formatValue } = this.slider;
+  onPointermove(event) {
+    event.preventDefault();
+    const { from, progress, to } = this.slider.subElements;
+    const values = this.getCurrentValues(event.clientX);
 
-    function getPercent(width, side) {
-      const leftStartPosition = this.startPosition - this.startOffsetX;
-      console.log(this.currentThumb.offsetWidth);
-      const rightStartPosition = this.startPosition + this.currentThumb.offsetWidth - this.startOffsetX;
-      const sideOptions = {
-        leftShiftPercent: (event.clientX - leftStartPosition) / width * 100,
-        rightShiftPercent: (event.clientX - rightStartPosition) / width * 100 * (-1),
-        leftPercent: this.minPercent,
-        rightPercent: this.maxPercent,
+    if (this.thumbSide === 'left') {
+      this.slider.selected.from = values.value;
+      from.innerHTML = this.slider.formatValue(values.value);
+      this.currentThumb.style.left = values.percent + '%';
+      progress.style.left = values.percent + '%';
+    } else {
+      this.slider.selected.to = values.value;
+      to.innerHTML = this.slider.formatValue(values.value);
+      this.currentThumb.style.right = (100 - values.percent) + '%';
+      progress.style.right = (100 - values.percent) + '%';
+    }
+  }
+
+  getCurrentValues(position) {
+    const { inner } = this.slider.subElements;
+    const { left: innerLeft, width } = inner.getBoundingClientRect();
+    const result = {};
+    const { min, max } = this.slider;
+    const range = max - min;
+
+    if (this.thumbSide === 'left') {
+      result.percent = (position - innerLeft + this.shiftX) / width * 100;
+    } else {
+      result.percent = (position - innerLeft - this.shiftX) / width * 100;
+    }
+
+    result.percent = this.checkLimit(result.percent);
+    result.value = range / 100 * result.percent + min;
+    result.value = Math.round(result.value);
+
+    return result;
+  }
+
+  checkLimit(percent) {
+    const left = parseFloat(this.slider.subElements.leftThumb.style.left);
+    const right = 100 - parseFloat(this.slider.subElements.rightThumb.style.right);
+
+    if (this.thumbSide === 'left') {
+      if (percent >= 0 && percent <= right) {
+        return percent;
+      } else {
+        return percent < 0 ? 0 : right;
       }
-      const result = sideOptions[`${side}Percent`] + sideOptions[`${side}ShiftPercent`];
-
-      return Math.round(checkLimits(side, result));
-    }
-      
-    function checkLimits(side, result) {
-      const maxLeft = 100 - maxPercent;
-      const maxRight = 100 - minPercent;
-
-      if (side === 'left') {
-        if (result >= 0 && result <= maxLeft) {
-          return result;
-        } else {
-          return result < 0 ? 0: maxLeft;
-        }
-      } else if (side === 'right') {
-        if (result <= maxRight && result >= 0) {
-          return result;
-        } else {
-          return result > maxRight ? maxRight : 0;
-        }
-      }
-    }
-
-    function getValue(percent, side) {
-      const normalPercent = side === 'left' ? percent : 100 - percent;
-      const result = min + (max - min) / 100 * normalPercent;
-
-      return Math.round(result);
-    }
-
-    if (this.currentThumb) {
-      const progressBase = this.currentThumb.offsetParent;
-      const side = this.currentThumb.className.replace('range-slider__thumb-', '');
-      if (progressBase) {
-        const currentPercent = getPercent.call(this, progressBase.offsetWidth, side);
-        const value = getValue(currentPercent, side);
-  
-        if (side === 'left') {
-          minValueElem.textContent = formatValue(value);
-          this.slider.minPercent = currentPercent;
-        } else if (side === 'right') {
-          maxValueElem.textContent = formatValue(value);
-          this.slider.maxPercent = currentPercent;
-        }
-  
-        this.currentThumb.style[side] = currentPercent + '%';
-        progressElem.style[side] = currentPercent + '%';
+    } else {
+      if (percent >= left && percent <= 100) {
+        return percent;
+      } else {
+        return percent < left ? left : 100;
       }
     }
   }
 
-  pointerup() {
-    if (this.currentThumb) {
-      this.currentThumb = null;
-      this.startPosition = 0;
-      this.startOffsetX = 0;
-      this.minPercent = 0;
-      this.maxPercent = 0;
-    }
+  onPointerup() {
+    document.removeEventListener('pointermove', this);
+    document.removeEventListener('pointerup', this);
+    this.currentThumb = null;
+    this.thumbSide = '';
+    this.shiftX = 0;
   }
 }
 
 export default class DoubleSlider {
   subElements = {};
-  listeners = ['pointerdown', 'pointermove', 'pointerup'];
 
-  constructor({min = 100, max = 200, formatValue = value => '$' + value,
-      selected = {}} = {}) {
-    const { from = min, to = max } = selected;
+  constructor({
+    min = 100,
+    max = 200,
+    formatValue = value => '$' + value,
+    selected = {
+      from: min,
+      to: max,
+    }} = {}) {
+    this.selected = selected;
     this.min = min;
     this.max = max;
-    this.minPercent = Math.round((from - min) / (max - min) * 100);
-    this.maxPercent = 100 - Math.round((to - min) / (max - min) * 100);
-    this.currentMinValue = this.getValue('left');
-    this.currentMaxValue = this.getValue('right');
     this.formatValue = formatValue;
     this.render();
-    this.initEventListeners();
   }
 
   initEventListeners() {
-    const worker = new Worker(this);
-    this.worker = worker;
-    this.listeners
-      .forEach(event => this.element.addEventListener(event, this.worker));
+    const handler = new Handler(this);
+    this.handler = handler;
+    this.subElements.leftThumb.addEventListener('pointerdown', this.handler);
+    this.subElements.rightThumb.addEventListener('pointerdown', this.handler);
   }
 
   removeEventListeners() {
-    this.listeners
-      .forEach(event => this.element.removeEventListener(event, this.worker));
+    document.removeEventListener('pointerdown', this.handler);
+    document.removeEventListener('pointerdown', this.handler);
   }
-  
+
   render() {
-    this.subElements.minValue = this.createValueElem('left');
-    this.subElements.maxValue = this.createValueElem('right');
-    this.subElements.progress = this.createProgressElem();
-    this.subElements.leftThumb = this.createThumbElem(this.minPercent, 'left');
-    this.subElements.rightThumb = this.createThumbElem(this.maxPercent, 'right');
-
-    const wrap = this.createWrapElem();
-    wrap.append(this.subElements.progress);
-    wrap.append(this.subElements.leftThumb);
-    wrap.append(this.subElements.rightThumb);
-
     const slider = this.createSlider();
-    slider.append(this.subElements.minValue);
-    slider.append(wrap);
-    slider.append(this.subElements.maxValue);
-
     this.element = slider;
+    this.subElements = this.getSubElements(slider);
+    this.setValues();
+    this.initEventListeners();
   }
 
   createSlider() {
     const slider = document.createElement('div');
-    slider.classList.add('range-slider');
+    slider.innerHTML = this.getTemplate();
 
-    return slider;
+    return slider.firstElementChild;
   }
 
-  createValueElem(side) {
-    const sideOptions = {
-      left: this.currentMinValue,
-      right: this.currentMaxValue,
-      leftAttr: 'from',
-      rightAttr: 'to'
-    }
-    const valueElem = document.createElement('span');
-    valueElem.dataset.element = sideOptions[`${side}Attr`];
-    const value = this.formatValue(sideOptions[side]);
-    valueElem.innerHTML = `<span>${value}</span>`;
+  getTemplate() {
+    const { from, to } = this.selected;
 
-    return valueElem;
+    return `<div class="range-slider">
+      <span data-element="from">${this.formatValue(from)}</span>
+      <div data-element="inner" class="range-slider__inner">
+        <span data-element="progress" class="range-slider__progress"></span>
+        <span data-element="leftThumb" class="range-slider__thumb-left"></span>
+        <span data-element="rightThumb" class="range-slider__thumb-right"></span>
+      </div>
+      <span data-element="to">${this.formatValue(to)}</span>
+    </div>`;
+  }
+
+  getSubElements(element) {
+    const elements = element.querySelectorAll('[data-element]');
+
+    return [...elements].reduce((accum, subElement) => {
+      accum[subElement.dataset.element] = subElement;
+
+      return accum;
+    }, {});
+  }
+
+  setValues() {
+    const left = this.getValue('left');
+    const right = this.getValue('right');
+
+    this.subElements.progress.style.left = left;
+    this.subElements.progress.style.right = right;
+    this.subElements.leftThumb.style.left = left;
+    this.subElements.rightThumb.style.right = right;
   }
 
   getValue(side) {
-    const normalPercent = side === 'left' ? this.minPercent : 100 - this.maxPercent;
-    const result = this.min + (this.max - this.min) / 100 * normalPercent;
+    const range = this.max - this.min;
+    const valueSide = {
+      left: this.selected.from - this.min,
+      right: this.max - this.selected.to,
+    }
 
-    return Math.round(result);
-  }
+    const result = valueSide[side] / range * 100;
 
-  createWrapElem() {
-    const wrapElem = document.createElement('div');
-    wrapElem.classList.add('range-slider__inner');
-
-    return wrapElem;
-  }
-
-  createProgressElem() {
-    const progressElem = document.createElement('span');
-    progressElem.classList.add('range-slider__progress');
-    progressElem.style = `left: ${this.minPercent}%; right: ${this.maxPercent}%`;
-
-    return progressElem;
-  }
-
-  createThumbElem(value, side) {
-    const thumbElem = document.createElement('span');
-    thumbElem.classList.add(`range-slider__thumb-${side}`);
-    thumbElem.style = `${side}: ${value}%`;
-
-    return thumbElem;
+    return Math.floor(result) + '%';
   }
 
   remove() {
